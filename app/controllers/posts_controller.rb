de9750623cfb8b6afba06d357411b5adb7263454
qpongratz <lagarathan@gmail.com>
2021-09-18 11:23:43 -0500

    Fill out posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if Post.save(post_params)
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+
+    if Post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end

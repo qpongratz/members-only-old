75ef384a4e4f89a82a8ea2b666e72e03ffef0ff9
qpongratz <lagarathan@gmail.com>
2021-09-18 12:25:36 -0500

    Fix new posts to associate with users'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController  
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @posts = Post.all
   end
@@ -8,11 +10,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to posts_path
@@ -44,6 +46,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
